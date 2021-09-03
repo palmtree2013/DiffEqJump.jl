753ab3d8982b0aaeed19b12eb260087468e816a6
xiaoming <palmtree2013@163.com>
2021-09-03 17:51:08 +0800

    solve need update

--- examples/test_Direct.jl
+++ examples/test_Direct.jl
@@ -13,14 +13,36 @@ end
 jump2 = ConstantRateJump(rate2,affect!)
 
 prob2 = DiscreteProblem([999.0,1.0,0.0],(0.0,250.0))
-jump_prob2 = JumpProblem(prob2,Direct(),jump1_,jump2, save_positions=(false,false),rng= Random.seed!(1234))
+jump_prob2 = JumpProblem(prob2,Direct(),jump1_,jump2, save_positions=(false,false),saveat = 1, rng= Random.seed!(1234))
 integrator = init(jump_prob2,FunctionMap())
-sol2 = solve(jump_prob2,SSAStepper(),saveat=1.)
-plot(sol2)
+# sol2 = solve(jump_prob2,SSAStepper(),saveat=1.)
+# plot(sol2)
+# jump_prob2.prob.p
+# integrator.u|>typeof
+integrator
+prob2.f
 
-jump_prob2.discrete_jump_aggregation
+integrator.saveat
 
-propertynames(jump_prob2)
+prob2.u0
+
+dja = jump_prob2.discrete_jump_aggregation
+dja(integrator)
+dja.next_jump
+integrator
+
+
+jump_prob2.discrete_jump_aggregation.next_jump
+
+propertynames(prob2)
+u0 = [999.0,1.0,0.0]
+rng = Random.seed!(1234)
+jumpset= JumpSet((),(jump2,),nothing,jump1_)
+
+DiffEqJump.aggregate(Direct(),u0,prob2.p
+,0.,250.,jumpset.constant_jumps,jumpset.massaction_jump,(false,false),rng)
+# u0
+
 jump_prob2|>propertynames
 jump_prob2.discrete_jump_aggregation|>propertynames
 jump_prob2.discrete_jump_aggregation.prev_jump
@@ -31,12 +53,20 @@ jump_prob2.prob|>propertynames
 jump_prob2.prob.f|propertynames
 # integrator.p
 
-
+integrator.f
 
 u = [999.,1.,0.]
 
 dja_ = jump_prob2.discrete_jump_aggregation
 DiffEqJump.execute_jumps!(dja_, integrator, integrator.u, integrator.p, integrator.t)
+
+dja_(integrator)
+dja_(0,u,0,integrator)
+
+propertynames(dja_)
+
+
+integrator
 
 integrator
 
